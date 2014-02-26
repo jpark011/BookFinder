91423c47663a203fed9d4849a249bb7261d9b4e4
wngt3319 <wngt3319@gmail.com>
2014-02-26 11:44:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+BookFinder
+==========
+This is a web application for LINKETT interaction at University of Toronto Bookstore.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookfinder::Application.config.secret_key_base = '2416adaff49670111bc714f22bed6af04b968ddb78f76c86a3a34854056f6cc54271cb1c005dd3aae1258474b04b955611b81be21229893bd9ee59cbd28cdd44'
